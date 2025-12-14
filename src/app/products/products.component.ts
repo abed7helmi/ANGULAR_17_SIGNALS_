080e5524d0d583aa4429fb8f756acfd9e30c471e
abed7helmi <abed.helmi@yahoo.fr>
2025-12-14 23:43:06 +0100

    products component with signals

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, computed, OnInit, signal} from '@angular/core';
 
 interface Product {
-  id : string,name : string,price : string,selected : boolean,
+  id : number,name : string,price : number,selected : boolean,
 }
 
 @Component({
@@ -9,18 +9,34 @@ interface Product {
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
-
-  products : Product[] = [
-    {id : '1',name :'Product 1',price :'551',selected:  true},
-    {id : '2',name :'Product 2',price :'232',selected:  true},
-    {id : '3',name :'Product 3',price :'53',selected:  true},
-  ];
+export class ProductsComponent implements OnInit{
+  products = signal<Product[]>([]) ;
 
 
+  constructor() {
+
+  }
+  ngOnInit() {
+    this.products.set(
+      [
+        {id : 1, name : "Computer", price : 6500, selected : false},
+        {id : 2, name : "Printer", price : 1200, selected : true},
+        {id : 3, name : "Smart phone", price : 3200, selected : true},
+      ]
+    );
+  }
+
+
+
+  selectedProducts = computed<number>(()=>this.products().filter(p=>p.selected).length)
+  total = computed<number>(()=>
+    this.products()
+      .filter(p=>p.selected)
+      .reduce((sum,current)=>sum+current.price,0))
 
   select(product: Product) {
-    product.selected = !product.selected;
-
+    this.products.update(prods=>{
+      return prods.map(p=>p.id==product.id?{...product, selected : !product.selected}: p);
+    });
   }
 }
